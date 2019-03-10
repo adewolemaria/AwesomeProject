897f77185750e4bd902a1f16411d85945fac0557
Mariya Hilton <marica-12@yandex.ru>
2019-03-10 16:37:37 +0300

    question page design changes

--- app/views/Qa.js
+++ app/views/Qa.js
@@ -1,101 +1,131 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ProgressViewIOS } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  ProgressViewIOS
+} from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import { LinearGradient } from 'expo'
+import { LinearGradient } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 export class Qa extends React.Component {
- static navigationOptions = {
+  static navigationOptions = {
     header: null
   };
 
   onPress = () => {
     Alert.alert('You tapped the button!');
-  }
-
+  };
 
   render() {
     return (
-      
-      <LinearGradient colors={['#0D0B18','#28263E']} style={styles.container}>
-        <Text style={styles.question}>After Jesus rose from the dead, how long did he remain on earth before ascending to heaven? </Text>
+      <LinearGradient colors={['#0D0B18', '#28263E']} style={styles.container}>
+        <View style={styles.question_container}>
+          <View style={styles.question_count}>
+            <Ionicons
+              name="md-arrow-round-back"
+              style={{
+                paddingLeft: '5%',
+                width: '35%'
+              }}
+              size={25}
+              color="#f3f3f3"
+            />
+            <Text style={styles.counter}>Question 1 of 10</Text>
+          </View>
+          <Text style={styles.question}>
+            After Jesus rose from the dead, how long did he remain on earth
+            before ascending to heaven?
+          </Text>
+        </View>
 
         <View style={styles.buttonRow}>
           <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}>
-            <Text style={styles.buttonText}>12 days</Text>
+            <Text style={styles.buttonText}>12 ddays</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}>
-            <Text style={styles.buttonText}>40 days</Text>
+            <Text style={styles.buttonText}>40 ddays</Text>
           </TouchableOpacity>
-        </View>
-        <View style={styles.buttonRow}>
-          <TouchableOpacity style={styles.buttonStyle}onPress={this.onPress}>
+          <TouchableOpacity style={styles.buttonStyle} onPress={this.onPress}>
             <Text style={styles.buttonText}>3 months</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.buttonSelectedStyle} onPress={this.onPress}>
+          <TouchableOpacity
+            style={styles.buttonSelectedStyle}
+            onPress={this.onPress}
+          >
             <Text style={styles.buttonTextSelected}>1 year</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.progressStyle}>
-          <ProgressViewIOS
-            style={styles.progressView}
-            progress={(0.2)}
-          />
+          <ProgressViewIOS style={styles.progressView} progress={0.2} />
         </View>
-        
       </LinearGradient>
     );
   }
-};
+}
 
-
-const styles = StyleSheet.create ({
- container: {
-   flex: 3,
- },
- question: {
-   paddingTop: '20%',
-   padding: 30,
-   flex: 1,
-   color: '#fff',
-   fontSize: 25
- },
-  buttonRow : {
-    flexDirection: 'row',
+const styles = StyleSheet.create({
+  container: {
+    flex: 3
+  },
+  counter: {
+    paddingVertical: 5,
+    textAlign: 'center',
+    color: 'white'
+  },
+  question: {
+    paddingRight: '5%',
+    paddingLeft: '5%',
+    width: '100%',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: 25
+  },
+  buttonRow: {
+    flexDirection: 'column',
     color: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    justifyContent: 'space-evenly'
+  },
+  question_count: {
+    flexDirection: 'row',
+    paddingVertical: 60
   },
   buttonStyle: {
-    backgroundColor: '#fff',
-    width: '40%',
-    height: '50%',
+    borderWidth: 1,
+    borderColor: '#d6006f',
+    width: '70%',
+    height: '15%',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10
   },
 
   buttonSelectedStyle: {
-    borderWidth: 1,
-    borderColor: '#d6006f',
-    width: '40%',
-    height: '50%',
+    backgroundColor: '#fff',
+    width: '70%',
+    height: '15%',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10
   },
   buttonText: {
-    color: '#000'
-  },
-  buttonTextSelected: {
     color: '#fff'
   },
+  buttonTextSelected: {
+    color: '#000'
+  },
 
-  progressView: { 
-    width: '90%',
+  progressView: {
+    width: '90%'
   },
   progressStyle: {
     padding: 20,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   }
 });
